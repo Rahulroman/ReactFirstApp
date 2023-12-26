c0436036b807701d72e0a314a660d275f17b5937
Rahul <you@example.comRahulshinde2144@gmail.com>
2023-12-26 17:19:25 +0530

    product

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProductList from "./Components/ProductList.tsx";
 
 function App() {
-  return <div className="App">Hello World...!!!</div>;
+  return (
+    <>
+      <ProductList />
+
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/"></Route>
+            <Route path="cart"></Route>
+          </Routes>
+        </Router>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/ProductList.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
+
+function ProductList() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((response) => {
+        setProducts(response.data.products);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
+  return (
+    <>
+      {products.map(function (data) {
+        return (
+          <div>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image={data.thumbnail}
+                title={data.title}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Model : {data.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default ProductList;
